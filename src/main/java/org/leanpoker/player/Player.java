9715a264e70396c8acc37fb8944e8c34274c36f4
Lars Eckart <lars.eckart@hey.com>
2023-06-07 13:41:03 -0400

    log stuff

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,13 +9,15 @@ public class Player {
 
     private static final Logger log = getLogger(Player.class);
 
-    static final String VERSION = "log current buy in and in action";
+    static final String VERSION = "log current buy in and in action and bet";
 
     public static int betRequest(JsonNode request) {
         int currentBuyIn = request.get("current_buy_in").asInt();
         log.info("current_buy_in: " + currentBuyIn);
         int inAction = request.get("in_action").asInt();
         log.info("in_action: " + inAction);
+        int currentBet = request.get("players").get(inAction).get("bet").asInt();
+        log.info("current_bet: " + currentBet);
         return 0;
     }
 
