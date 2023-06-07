d71d5532e16d0283cdb8f6645850ccf7b2987bd1
Lars Eckart <lars.eckart@hey.com>
2023-06-07 13:38:53 -0400

    log stuff

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,12 +1,21 @@
 package org.leanpoker.player;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import org.slf4j.Logger;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 public class Player {
 
-    static final String VERSION = "Default Java folding player";
+    private static final Logger log = getLogger(Player.class);
+
+    static final String VERSION = "log current buy in and in action";
 
     public static int betRequest(JsonNode request) {
+        int currentBuyIn = request.get("current_buy_in").asInt();
+        log.info("current_buy_in: " + currentBuyIn);
+        int inAction = request.get("in_action").asInt();
+        log.info("in_action: " + inAction);
         return 0;
     }
 
